f041e21d53feb43999f79301ca50910f78df7be1
Hendi Santika <hendisantika@gmail.com>
2017-09-05 08:18:23 +0700

    #10 - Modified pom.xml

---
+++ src/main/java/com/hendisantika/swaggerexample/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.hendisantika.swaggerexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/5/17
+ * Time: 7:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Employee API Example")
+                .description("A implementation of an API Gateway for Keyhole Labs Microservice Reference.")
+                .contact(new Contact("Keyhole Software", "hendisantika", "hendisantika@yahoo.co.id"))
+                .version("2.0")
+                .build();
+    }
+}
