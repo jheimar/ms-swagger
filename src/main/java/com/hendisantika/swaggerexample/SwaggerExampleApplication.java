8e0ba7e78582a80d2da0151896f8221879b222e9
Hendi Santika <hendisantika@gmail.com>
2017-09-05 07:39:15 +0700

    #2 - Added comments

--- src/main/java/com/hendisantika/swaggerexample/SwaggerExampleApplication.java
+++ src/main/java/com/hendisantika/swaggerexample/SwaggerExampleApplication.java
@@ -3,6 +3,16 @@ package com.hendisantika.swaggerexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/17
+ * Time: 7:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
 @SpringBootApplication
 public class SwaggerExampleApplication {
 
