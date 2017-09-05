d91e3e15394372b7a9200729a4c75e3883ef1e06
Hendi Santika <hendisantika@gmail.com>
2017-09-06 06:23:11 +0700

    #17 - Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into employee(id, firstname, lastname, email) values (1, 'John', 'Doe', 'johndoe@email.com');
+insert into employee(id, firstname, lastname, email) values (2, 'Hendi', 'Santika', 'hendisantika@email.com');
+insert into employee(id, firstname, lastname, email) values (3, 'David', 'Hasselholf', 'davidhasselholf@email.com');
+insert into employee(id, firstname, lastname, email) values (4, 'Uzumaki', 'Naruto', 'uzumakinaruto@konohagakure.com');
+insert into employee(id, firstname, lastname, email) values (5, 'Sakura', 'Haruno', 'sakuraharuno@konohagakure.com');
+insert into employee(id, firstname, lastname, email) values (6, 'Uchiha', 'Sasuke', 'uchihasasuke@konohagakure.com');
+insert into employee(id, firstname, lastname, email) values (7, 'Hatake', 'Kakashi', 'hatakekakashi@konohagakure.com');
+insert into employee(id, firstname, lastname, email) values (8, 'Namikaze', 'Minato', 'namikazeminato@konohagakure.com');
