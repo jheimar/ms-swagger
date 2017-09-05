470b9d01dde63e6a0e9fbc8b73686081927a3c2a
Hendi Santika <hendisantika@gmail.com>
2017-09-05 10:00:30 +0700

    #11 - Modified SwaggerConfig.java

---
+++ src/main/java/com/hendisantika/swaggerexample/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.hendisantika.swaggerexample.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/5/17
+ * Time: 9:59 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmployeeController {
+}
