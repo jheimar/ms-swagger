781c5bba2a26dce0073dd7bd51894385ed737592
Hendi Santika <hendisantika@gmail.com>
2017-09-05 07:42:47 +0700

    #3 - Added Employee Model

---
+++ src/main/java/com/hendisantika/swaggerexample/model/Employee.java
@@ -0,0 +1,56 @@
+package com.hendisantika.swaggerexample.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/5/17
+ * Time: 7:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
